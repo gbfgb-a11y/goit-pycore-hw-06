a56e1f6368543cb5cfb5b8a016781bc880d3a6c3
Yehor_9767624362362738q9!@ <yk.cm.sb@gmail.com>
2025-11-01 13:05:39 +0200

    Update task_main.py

--- task_main.py
+++ task_main.py
@@ -1,8 +1,10 @@
 from collections import UserDict
 
 class Field:
+
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return str(self.value)
 
@@ -10,16 +12,21 @@ class Name(Field):
     pass
 
 class Phone(Field):
+
     def __init__(self, value):
         if not isinstance(value, str):
             raise TypeError("Phone must be a string")
         if not value.isdigit() or len(value) != 10:
             raise ValueError("Phone number must contain exactly 10 digits")
         super().__init__(value)
+
+
     def __eq__(self, other):
         return isinstance(other, Phone) and self.value == other.value
 
+
 class Record:
+
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
@@ -30,31 +37,43 @@ class Record:
         self.phones.append(Phone(phone))
 
     def remove_phone(self, phone):
-        for i in self.phones:
-            if i.value == phone:
-                self.phones.remove(i)
-                break
+        if isinstance(phone, Phone):
+            phone = phone.value
+        isornot2 = self.find_phone(phone)
+        if isornot2 is not None:
+            self.phones.remove(isornot2)
+        else:
+            raise ValueError('Phone does not exist.')
+        
 
     def edit_phone(self, old_phone, new_phone):
         for i, a in enumerate(self.phones):
             if a.value == old_phone:
-                self.phones[i] = Phone(new_phone)
-                break
+                self.remove_phone(old_phone)
+        self.add_phone(new_phone)
+
 
     def find_phone(self, phone):
         for i in self.phones:
             if i.value == phone:
                 return i
         return None
+    
 
     def __str__(self):
         return f"Contact name: {self.name}, phones: {'; '.join(str(p) for p in self.phones)}"
 
+
 class AddressBook(UserDict):
+
     def add_record(self, record):
         self.data[record.name.value] = record
+
+
     def find(self, name):
         return self.data.get(name)
+    
+
     def delete(self, name):
         if name in self.data:
             del self.data[name]
